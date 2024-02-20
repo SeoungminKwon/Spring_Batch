d29d265a12668f0169915ceac8686f4dfd235bed
SeoungminKwon <tjdalsrnjs10@gmail.com>
2024-02-20 09:36:39 +0900

    feat: makeProductLogJob을 batch컨트롤러를 통해서 실행

--- src/main/java/com/ll/sbbatch/batch/BatchController.java
+++ src/main/java/com/ll/sbbatch/batch/BatchController.java
@@ -7,6 +7,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 @Controller
 @RequestMapping("/batch")
 @RequiredArgsConstructor
@@ -17,7 +20,16 @@ public class BatchController {
     @ResponseBody
     public String runHelloJob(){
         batchService.runHelloJobJob();
-
         return "runHelloJob OK";
+    }
+
+    @GetMapping("/makeProductLog")
+    @ResponseBody
+    public String runMakeProductLogJob(){
+        LocalDateTime startDate = LocalDateTime.now().withHour(0).withMinute(0).withSecond(0).withNano(0);
+        LocalDateTime endDate = LocalDateTime.now().withHour(0).withMinute(0).withSecond(0).withNano(0);
+
+        batchService.runMakeProductLogJob(startDate, endDate);
+        return "makeProductLog OK";
     }
 }
--- src/main/java/com/ll/sbbatch/batch/BatchService.java
+++ src/main/java/com/ll/sbbatch/batch/BatchService.java
@@ -7,6 +7,8 @@ import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+
 @Service
 @RequiredArgsConstructor
 public class BatchService {
@@ -14,6 +16,7 @@ public class BatchService {
     //스프링 배치 작업을 실행하는 인터페이스, Job실행, 필요한 매개변수를 전달 할 수 있다.
     private final JobLauncher jobLauncher;
     private final Job helloJob;
+    private final Job makeProductLogJob;
 
     public void runHelloJobJob() {
         try {
@@ -21,6 +24,22 @@ public class BatchService {
                     .toJobParameters();
             jobLauncher.run(helloJob, jobParameters);
         } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void runMakeProductLogJob(LocalDateTime startDate_, LocalDateTime endDate_) {
+
+        try{
+            String startDate = startDate_.toString().substring(0, 10) + " 00:00:00.000000";
+            String endDate = endDate_.toString().substring(0, 10) + " 23:59:59.999999";
+
+            JobParameters jobParameters = new JobParametersBuilder()
+                    .addString("startDate", startDate)
+                    .addString("endDate", endDate)
+                    .toJobParameters();
+            jobLauncher.run(makeProductLogJob, jobParameters);
+        } catch (Exception e){
             e.printStackTrace();
         }
     }
